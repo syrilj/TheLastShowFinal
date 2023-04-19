01b9f63727f0df758e334a669473268979a749cd
Hamza Amar <rhamzax@gmail.com>
2023-04-19 03:05:31 -0600

    Get all items corresponding to uuid

--- functions/get-obituaries/main.py
+++ functions/get-obituaries/main.py
@@ -1,1 +1,26 @@
-# add your get-obituaries function here
+import boto3
+from boto3.dynamodb.conditions import Key
+
+# create a dynamodb resource
+dynamodb_resource = boto3.resource("dynamodb", region_name="ca-central-1")
+table_name = "lastshow-30144227"
+
+# create a dynamodb table object
+table = dynamodb_resource.Table(table_name)
+
+def lambda_handler(event, context):
+    http_method = event["requestContext"]["http"]["method"].lower()
+    obituaries = []
+    if http_method == "get":
+
+        uuid = event["queryStringParameters"]["uuid"]
+          
+        response = table.query(
+            TableName = table_name,
+            KeyConditionExpression=Key("uuid").eq(uuid)
+        )
+        obituaries = response["Items"]
+        
+    return obituaries
+
+    
