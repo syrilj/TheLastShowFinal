243ae53f3779b24147a6fcde9f0a78690933e30a
Hamza Amar <rhamzax@gmail.com>
2023-04-19 01:12:19 -0600

    Add all infastructure code

--- infra/main.tf
+++ infra/main.tf
@@ -15,3 +15,142 @@ provider "aws" {
 # one dynamodb table
 # roles and policies as needed
 # step functions (if you're going for the bonus marks)
+
+
+# read the docs: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/dynamodb_table
+resource "aws_dynamodb_table" "lastshow-30144227" {
+  name         = "lastshow-30144227"
+  billing_mode = "PROVISIONED"
+
+  # up to 8KB read per second (eventually consistent)
+  read_capacity = 1
+
+  # up to 1KB per second
+  write_capacity = 1
+
+  #Need to change these values for the new dynamodb
+  # hash_key = "email"
+  # range_key = "note_id"
+
+  # attribute {
+  #   name = "note_id"
+  #   type = "S"
+  # }
+  # attribute {
+  #   name = "email"
+  #   type = "S"
+  # }
+
+}
+
+
+resource "aws_iam_role" "iam_for_lambda" {
+  name = "iam_for_lambda"
+
+  assume_role_policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Principal" : {
+          "Service" : "lambda.amazonaws.com"
+        },
+        "Action" : "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "dynamodb-lambda-policy" {
+  name = "dynamodb_lambda_policy"
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Action" : ["dynamodb:*"],
+        "Resource" : "${aws_dynamodb_table.lastshow-30144227.arn}"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "attach" {
+
+  role = aws_iam_role.iam_for_lambda.name
+  policy_arn = aws_iam_policy.dynamodb-lambda-policy.arn
+
+}
+
+data "archive_file" "get-obituaries-archive" {
+  source_file = "../functions/get-obituaries/main.py"
+  output_path = "get-notes.zip"
+  type        = "zip"
+}
+
+
+resource "aws_lambda_function" "get-obituaries" {
+  role          = aws_iam_role.iam_for_lambda.arn
+  handler       = "main.lambda_handler"
+  function_name = "get-obituaries-30145947"
+  filename      = "get-obituaries.zip"
+  source_code_hash = data.archive_file.get-obituaries-archive.output_base64sha256
+
+  runtime = "python3.9"
+}
+
+# create a Function URL for Lambda 
+# see the docs: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/lambda_function_url
+resource "aws_lambda_function_url" "get-obituaries-url" {
+  function_name      = aws_lambda_function.get-obituaries.function_name
+  authorization_type = "NONE"
+
+  cors {
+    allow_credentials = true
+    allow_origins     = ["*"]
+    allow_methods     = ["GET"]
+    allow_headers     = ["*"]
+    expose_headers    = ["keep-alive", "date"]
+  }
+}
+
+# show the Function URL after creation
+output "get-obituaries_url" {
+  value = aws_lambda_function_url.get-obituaries-url.function_url
+}
+
+data "archive_file" "create-obituary-archive" {
+  source_file = "../functions/create-obituary/main.py"
+  output_path = "create-obituary.zip"
+  type        = "zip"
+}
+
+resource "aws_lambda_function" "create-obituary" {
+  role          = aws_iam_role.iam_for_lambda.arn
+  handler       = "main.lambda_handler"
+  function_name = "create-obituary-30145947"
+  filename      = "create-obituary.zip"
+  source_code_hash = data.archive_file.create-obituary-archive.output_base64sha256
+
+  runtime = "python3.9"
+}
+
+# create a Function URL for Lambda 
+# see the docs: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/lambda_function_url
+resource "aws_lambda_function_url" "create-obituary-url" {
+  function_name      = aws_lambda_function.create-obituary.function_name
+  authorization_type = "NONE"
+
+  cors {
+    allow_credentials = true
+    allow_origins     = ["*"]
+    allow_methods     = ["POST"]
+    allow_headers     = ["*"]
+    expose_headers    = ["keep-alive", "date"]
+  }
+}
+
+# show the Function URL after creation
+output "create-obituary_url" {
+  value = aws_lambda_function_url.create-obituary-url.function_url
+}
